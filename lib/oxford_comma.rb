8a1b246c691a27dc109cdc4c2de4cb10eb15d830
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 17:49:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,10 @@ def oxford_comma(array)
     array.join(' and ')
 
   else
-    array.join(', ')
+    array2 = array[0..array.length-3]
+    array3 = array[-2,2]
+
+    return "#{array2.join(", ")}, #{array3.join(" and ")}"
 
   end
 end
