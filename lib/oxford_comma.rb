aa9b3ffdf7de93110aa16b01773e21cd8d44abe1
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 17:50:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
 
+  elsif array.length == 2
+    array.join(' and ')
+
+  else
+    array2 = array[0..array.length-3]
+    array3 = array[-2,2]
+
+    return "#{array2.join(", ")}, #{array3.join(", and ")}"
+
+  end
 end
+
